940dc6c442a2a0308591a53cff138bbb8e8fdb30
heahgo <h34hg0@gmail.com>
2023-09-15 11:08:43 +0900

    Move pad, unpad methods to padding.py

--- Crypto/Cipher/AES/AES_util.py
+++ Crypto/Cipher/AES/AES_util.py
@@ -1,16 +1,3 @@
-def pad(byte, block_size):
-    pad_byte =  bytes({block_size - (len(byte) % block_size)})
-    return byte + pad_byte[0] * pad_byte
-
-def unpad(byte, block_size):
-    pad_bytes = byte[-1]
-    if pad_bytes > block_size or pad_bytes == 0:
-        raise ValueError
-    for i in range(pad_bytes):
-        if byte[-1] != pad_bytes:
-            raise ValueError
-    return byte[:-(pad_bytes)]
-
 def byte2list(byte):
         return [byte[4*i + j] for i in range(4) for j in range(4)]
 
